be018b77a0461fec4fb469b1de96285b52d5a8b8
saalcazar <alcazar.samuel@gmail.com>
2023-02-05 01:45:01 -0400

    Commit de prueba

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 const Home = () => {
     return(
-        <h1>INDEX</h1>
+        <div>
+            <h1>Index</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptates exercitationem rerum quod, ex error iusto asperiores reprehenderit alias! Perferendis, nobis, sapiente, quo id fugit quos necessitatibus ut molestias culpa cumque nesciunt ipsa. Eius, delectus rem molestias ex voluptates necessitatibus suscipit vitae. Nam natus minus quam reprehenderit sapiente sed voluptas.</p>
+        </div>
     )
 }
 
