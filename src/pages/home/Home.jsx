ba9ca33244d49ca69b966071d9ae3ae3a33d7601
saalcazar <alcazar.samuel@gmail.com>
2023-02-05 01:58:29 -0400

    Update Home.jsx
    Commit desde mi laptop

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
         <div>
             <h1>Index</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptates exercitationem rerum quod, ex error iusto asperiores reprehenderit alias! Perferendis, nobis, sapiente, quo id fugit quos necessitatibus ut molestias culpa cumque nesciunt ipsa. Eius, delectus rem molestias ex voluptates necessitatibus suscipit vitae. Nam natus minus quam reprehenderit sapiente sed voluptas.</p>
+            <a href="https://ceadl.org.bo"></a>
         </div>
     )
 }
