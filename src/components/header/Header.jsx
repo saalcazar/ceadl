9f5ad809723b2c3a62cd64fe4c6d5e1f02e6eb11
saalcazar <alcazar.samuel@gmail.com>
2023-02-06 12:10:04 -0400

    Cambios con el CEADL

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import { NavLink, useNavigate } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo-tres.svg'
 import "./Header.css"
 const Header = () => {
     const navigation = useNavigate()
