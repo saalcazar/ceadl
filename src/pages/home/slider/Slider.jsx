c0d6746c891b22da59919d5ff8c0b00899addf5f
saalcazar <alcazar.samuel@gmail.com>
2023-02-08 01:34:09 -0400

    Slider casi terminado

--- src/pages/home/slider/Slider.jsx
+++ src/pages/home/slider/Slider.jsx
@@ -1,14 +1,57 @@
-import sliderUno from './img/mujeres.jpg'
+import { useEffect, useState } from 'react'
 import Slide from './slide/Slide'
 import "./Slider.css"
+
+const slide = [
+    {
+        id: 0,
+        img: '../../../../../src/pages/home/slider/img/mujeres.jpg', 
+        title :"Mujer y economía",
+        p:"Las mujeres han estado históricamente subrepresentadas en la economía, ya sea en el empleo o en la propiedad de negocios. Sin embargo, en las últimas décadas, se ha hecho un esfuerzo para aumentar la participación de las mujeres en el mundo laboral y económico."
+    },
+
+    {
+        id: 1,
+        img: '../../../../../src/pages/home/slider/img/violencia.jpg', 
+        title: "Violencia de género",
+        p: "Las mujeres han estado históricamente subrepresentadas en la economía, ya sea en el empleo o en la propiedad de negocios. Sin embargo, en las últimas décadas, se ha hecho un esfuerzo para aumentar la participación de las mujeres en el mundo laboral y económico."
+    },
+    {
+        id: 2,
+        img: '../../../../../src/pages/home/slider/img/lgbt.jpg', 
+        title: "Por el derecho a la inclución",
+        p: "Las mujeres han estado históricamente subrepresentadas en la economía, ya sea en el empleo o en la propiedad de negocios. Sin embargo, en las últimas décadas, se ha hecho un esfuerzo para aumentar la participación de las mujeres en el mundo laboral y económico."
+    }
+
+]
+
 const Slider = () => {
+
+    const [currentSlide, setCurrentSlide] = useState(0)
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentSlide((currentSlide + 1) % slide.length);
+        }, 5000);
+        return () => clearInterval(intervalId);
+    }, [currentSlide, slide.length])
+
     return (
         <section className="main-slider">
-            <Slide
-                img={sliderUno} 
-                title="Mujer y economía"
-                p="Las mujeres han estado históricamente subrepresentadas en la economía, ya sea en el empleo o en la propiedad de negocios. Sin embargo, en las últimas décadas, se ha hecho un esfuerzo para aumentar la participación de las mujeres en el mundo laboral y económico."
-            />
+            <div className="content-slider">
+                {
+                    slide.map(({id, img, title, p}) => (
+                        <Slide
+                            key={id}
+                            id={id}
+                            img={img}
+                            title={title}
+                            p={p}
+                            classSlider={`slider active`}
+                        />
+                    ))[currentSlide]
+                }
+            </div>
         </section>
     )
 }
--- src/pages/home/slider/slide/Slide.jsx
+++ src/pages/home/slider/slide/Slide.jsx
@@ -1,20 +1,23 @@
-const Slide = ({img, p, title}) => {
+import { useEffect, useState } from "react";
+
+const Slide = (props) => {
+
     return(
         <>
-            <div className="slider">
+            <div key={props.id} className={props.classSlider}>
                 <div className='img-slider img-container center'>
-                    <img src={img} alt="Mujer y economía" />
+                    <img src={props.img} alt={props.img} />
                 </div>
                 <article className="container article-slide center">
-                    <h2 className="title-light title-slider">{title}</h2>
+                    <h2 className="title-light title-slider">{props.title}</h2>
                     <div className="contrast between">
-                        <span className="paragraph-light slider-arrow">&#10094;</span>
-                        <p className="paragraph-light p-slider">{p}</p>
-                        <span className="paragraph-light slider-arrow">&#10095;</span>
+                        <span className="paragraph-light slider-arrow" id="left-arrow">&#10094;</span>
+                        <p className="paragraph-light p-slider">{props.p}</p>
+                        <span className="paragraph-light slider-arrow" id="right-arrow">&#10095;</span>
                     </div>
                     <span className="button-dark read-more"><a className="a-light a-slide" href="">Leer más...</a></span>
                     <div className="slider-navigation center">
-                        <span className="round-slider"></span>
+                        <span className="round-slider round-active"></span>
                         <span className="round-slider"></span>
                         <span className="round-slider"></span>
                     </div>
