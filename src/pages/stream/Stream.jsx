f864bc0090e1f1a68ab7143a812416ce1cfecfd9
saalcazar <alcazar.samuel@gmail.com>
2023-03-03 05:17:38 -0400

    stream

--- src/pages/stream/Stream.jsx
+++ src/pages/stream/Stream.jsx
@@ -4,20 +4,37 @@ const Stream = () => {
     const srcUrl = `https://www.youtube.com/embed/${videoId}`;
     return(
         <section className="main-stream">
-            <div className="container between">
-                <div>
-                <iframe
-                    title="video de YouTube"
-                    width="560"
-                    height="315"
-                    src={srcUrl}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
+            <div className="container between stream">
+                <div className='streaming'>
+                    <iframe
+                        title="video de YouTube"
+                        width="560"
+                        height="315"
+                        src={srcUrl}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        className='video-stream'
+                    ></iframe>
+                    <article className='article-stream'>
+                        <p className='paragraph-light'>
+                        El objetivo de este curso es proporcionar información y herramientas para prevenir y denunciar la violencia intrafamiliar. La violencia intrafamiliar incluye cualquier forma de abuso o agresión cometido por un miembro de la familia o pareja hacia otro.
+
+                        Algunos de los temas que se abordarán en este curso incluyen:
+                        </p>
+                        <ul className='paragraph-light list-stream'>
+                            <li>Definición de la violencia intrafamiliar</li>
+                            <li>Tipos de violencia intrafamiliar, incluyendo física, psicológica, sexual y económica</li>
+                            <li>Factores que contribuyen a la violencia intrafamiliar</li>
+                            <li>Cómo prevenir la violencia intrafamiliar</li>
+                            <li>Cómo denunciar la violencia intrafamiliar, incluyendo los recursos disponibles y cómo acceder a ellos</li>
+                            <li>Cómo apoyar a víctimas de la violencia intrafamiliar</li>
+                        </ul>
+
+                    </article>
                 </div>
                 <div>
-                <div class="fb-comments" data-href="https://saalcazar.org" data-width="500" data-numposts="5"></div>
+
                 </div>
             </div>
         </section>
